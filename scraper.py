886f18e9e5a153a60b8e88b3c69ee648a42a8932
NETANEL-RABI <n0527648116@gmail.com>
2026-04-26 22:43:26 +0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,8 +4,8 @@ import json
 # רשימת מקורות RSS (תוכל להוסיף עוד בהמשך)
 rss_feeds = [
     "https://rss.walla.co.il/feed/1", # מבזקי וואלה
-    "https://www.ynet.co.il/Integration/StoryRss2.xml" # מבזקי Ynet
-    https://www.emess.co.il/
+    ""https://www.ynet.co.il/Integration/StoryRss2.xml",
+"https://www.emess.co.il/feed/"
     
 ]
 
